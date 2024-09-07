d9f58d101a24a62d95afcdec2e9625b86795292f
teqnok <teqnok@proton.me>
2024-09-07 17:06:13 -0500

    upd

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,7 +1,7 @@
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --prompt='=> ' --preview-window='border-sharp' --height=~20%"
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --prompt='=> ' --tmux --preview-window='border-sharp' --height=~40%"
 
 eval "$(fzf --zsh)"
 eval "$(zoxide init zsh)"
@@ -20,32 +20,58 @@ alias getpkg="sudo pacman -S"
 alias delpkg="sudo pacman -R"
 alias Delpkg="sudo pacman -Rns" 
 alias catpkg="yay -Si"
+# gif made with vhs (github.com/charmbracelet/vhs)
+# dependencies: fzf
 function findpkg() {
-    pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | while read -r pkg; do
-    action=$(echo -e "Install\nRemove\nUpdate\nReinstall" | fzf --prompt "Choose action for $pkg:")
-
+    action=$(echo -e "Cancel\nUpdate System\nUpdate Mirrors\nManage Packages" | fzf --prompt "Choose an action: " -m)
+    
     case "$action" in
-        "Install")
-            echo "Installing $pkg..."
-            sudo pacman -S "$pkg"
+        "Update System")
+            echo "Updating system..."
+            sudo pacman -Syu --noconfirm
             ;;
-        "Remove")
-            echo "Removing $pkg..."
-            sudo pacman -R "$pkg"
+        "Update Mirrors")
+            echo "Updating mirrors..."
+            sudo pacman -Sy
             ;;
-        "Update")
-            echo "Updating $pkg..."
-            sudo pacman -Syu "$pkg"
+        "Manage Packages")
+            selected_packages=$(pacman -Slq | fzf --multi --preview 'pacman -Si {1}' --prompt "Packages => ")
+            trimmed=$(echo "$selected_packages" | tr '\n' ' ')
+            if [[ -z "$selected_packages" ]]; then
+                echo "No packages selected."
+                return
+            fi
+
+            action=$(echo -e "Cancel\nInstall\nRemove\nReinstall" | fzf --prompt "Choose action for package(s) => " --header "Selected Packages: | $trimmed|" -m)
+
+            case "$action" in
+                "Install")
+                    echo "Installing packages..."
+                    echo "$selected_packages" | xargs -r sudo pacman -S --noconfirm
+                    ;;
+                "Remove")
+                    echo "Removing packages..."
+                    echo "$selected_packages" | xargs -r sudo pacman -R --noconfirm
+                    ;;
+                "Reinstall")
+                    echo "Reinstalling packages..."
+                    echo "$selected_packages" | xargs -r sudo pacman -S --overwrite --noconfirm
+                    ;;
+                "Cancel")
+                    echo "Operation cancelled."
+                    ;;
+                *)
+                    echo "No action selected or invalid action."
+                    ;;
+            esac
             ;;
-        "Reinstall")
-            echo "Reinstalling $pkg..."
-            sudo pacman -S --overwrite "$pkg"
+        "Cancel")
+            echo "Operation cancelled."
             ;;
         *)
             echo "No action selected or invalid action."
             ;;
     esac
-done
 }
 
 alias fdpkg=findpkg
